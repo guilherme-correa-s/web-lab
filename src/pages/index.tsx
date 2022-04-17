698b80c8e0c1edc55799e22910bc95e05476d762
guilherme correa <guilhermescorrea@hotmail.com.br>
2022-04-17 10:55:54 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import { getSession, getAccessToken } from "@auth0/nextjs-auth0";
+import { GetServerSideProps, NextPage } from "next";
+
+const Home: NextPage = () => {
+  return null;
+};
+
+export default Home;
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = getSession(req, res);
+  const token = getAccessToken(req, res);
+  console.log(token)
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/api/auth/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    redirect: {
+      destination: "/app",
+      permanent: false,
+    },
+  };
+};
